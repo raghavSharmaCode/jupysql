bde72b96194f816cb782883f248dd684bfef55f6
Palash Shah <35114859+Palashio@users.noreply.github.com>
2023-03-06 22:32:09 -0500

    modified logic

--- src/sql/magic_cmd.py
+++ src/sql/magic_cmd.py
@@ -76,38 +76,59 @@ class SqlCmdMagic(Magics, Configurable):
                 "-c", "--column", type=str, help="Column name", required=False
             )
             parser.add_argument(
-                "-w",
-                "--within",
-                type=str,
-                help="Whether it is within two numbers",
-                required=False,
-            )
-            parser.add_argument(
-                "-sg",
-                "--strictly-greater-than",
+                "-g",
+                "--greater",
                 type=str,
                 help="Greater than a certain number.",
                 required=False,
             )
             parser.add_argument(
-                "-sl",
-                "--strictly-less-than",
+                "-goe",
+                "--greater-or-equal",
+                type=str,
+                help="Greater or equal than a certain number.",
+                required=False,
+            )
+            parser.add_argument(
+                "-l",
+                "--less-than",
                 type=str,
                 help="Less than a certain number.",
+                required=False,
+            )
+            parser.add_argument(
+                "-loe",
+                "--less-than-or-equal",
+                type=str,
+                help="Less than or equal to a certain number.",
                 required=False,
             )
             parser.add_argument(
                 "-nn",
                 "--no-nulls",
                 help="Returns rows in specified column that are not null.",
-                action="store_false",
+                action="store_true",
             )
 
             args = parser.parse_args(others)
+
+            if args.greater and args.greater_or_equal:
+                return ValueError(
+                    "You cannot use both greater and greater than or equal to arguments at the same time."
+                )
+            elif args.less_than and args.less_than_or_equal:
+                return ValueError(
+                    "You cannot use both less and less than or equal to arguments at the same time."
+                )
+
             query = construct_string_query(args)
             conn = sql.connection.Connection.current.session
             res = conn.execute(query).fetchall()
-            return res
+
+            if args.no_nulls and len(res) > 0:
+                raise ValueError(
+                    "Specified column {} has null values present.".format(args.column)
+                )
         else:
             raise UsageError(
                 f"%sqlcmd has no command: {cmd_name!r}. "
@@ -117,17 +138,23 @@ class SqlCmdMagic(Magics, Configurable):
 
 def construct_string_query(args):
     base_query = select("*").from_(args.table)
-    if args.within:
-        bottom, top = args.within.split(",")
-        where = condition(args.column + "<" + top).and_(args.column + ">" + bottom)
+
+    if args.greater:
+        where = condition(args.column + ">" + args.greater)
         base_query = base_query.where(where)
-    if args.strictly_greater_than:
-        where = condition(args.column + ">" + args.strictly_greater_than)
+    if args.greater_or_equal:
+        where = condition(args.column + ">=" + args.greater_or_equal)
         base_query = base_query.where(where)
-    if args.strictly_less_than:
-        where = condition(args.column + "<" + args.strictly_less_than)
+    if args.less_than:
+        where = condition(args.column + "<" + args.less_than)
         base_query = base_query.where(where)
+    if args.less_than_or_equal:
+        where = condition(args.column + "<=" + args.less_than_or_equal)
+        base_query = base_query.where(where)
+    base_query = base_query.sql()
+
     if args.no_nulls:
-        where = condition("IS NOT NULL")
-        base_query = base_query.where(where)
-    return base_query.sql()
+        not_null = condition(args.column + "=NULL")
+        base_query = base_query + " AND " + args.column + " IS NOT NULL"
+
+    return base_query
