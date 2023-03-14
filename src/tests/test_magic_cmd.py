7a6fe865c672a2909c0da17afdc4558c21f46459
Palash Shah <35114859+Palashio@users.noreply.github.com>
2023-03-14 18:11:39 -0400

    move test

--- src/tests/test_magic_cmd.py
+++ src/tests/test_magic_cmd.py
@@ -70,13 +70,3 @@ ATTACH DATABASE 'my.db' AS some_schema
     ).result._repr_html_()
 
     assert "some_number" in out
-
-
-def test_sql_cmd_magic_uno(ip):
-    result = ip.run_cell(
-        "%sqlcmd test --table author --column year_of_death"
-        " --less-than 1700 --greater 1600"
-    ).result
-
-    assert len(result) == 2
-    assert "William" in str(result["less_than"])
